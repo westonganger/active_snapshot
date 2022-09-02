ed7a91333f26651fe7060eda3115843c34a6f04e
Oskar Gargas <og@gatenox.com>
2022-09-02 13:09:26 +0200

    #15 PR: Use strings instead of symbols for configuration

--- lib/active_snapshot/config.rb
+++ lib/active_snapshot/config.rb
@@ -1,25 +1,28 @@
+# frozen_string_literal: true
+
 class ActiveSnapshot::Config
   class << self
     attr_reader :storage_method
   end
 
   def self.setup(&block)
-    self.storage_method = :yaml
+    self.storage_method = 'yaml'
     instance_eval(&block) if block_given?
   end
 
   def self.storage_method=(value)
-    if [:yaml, :json].include?(value)
-      @storage_method = value
+    value_str = value.to_s
+    if ['yaml', 'json'].include?(value_str)
+      @storage_method = value_str
     end
   end
 
   def self.storage_method_yaml?
-    @storage_method == :yaml
+    @storage_method == 'yaml'
   end
 
   def self.storage_method_json?
-    @storage_method == :json
+    @storage_method == 'json'
   end
 end
 
