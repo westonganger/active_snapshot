f424d999b34443a6cf9932217d53a54a2998674c
Weston Ganger <weston@westonganger.com>
2023-02-18 10:32:32 -0800

    Utilize ActiveRecord lazy loading

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 #$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 ENV["RAILS_ENV"] = "test"
 
+require "active_support/all"
+
 require "active_snapshot"
 
 if ENV["ACTIVE_SNAPSHOT_STORAGE_METHOD"].present?
