19ddbdb73c6c50d4d01db8780ae07cde71f797fe
Weston Ganger <weston@westonganger.com>
2023-02-18 21:19:09 -0800

    Remove redundant SnapshopItem item_type uniqueness validation

--- lib/active_snapshot/models/snapshot_item.rb
+++ lib/active_snapshot/models/snapshot_item.rb
@@ -13,7 +13,7 @@ module ActiveSnapshot
 
     validates :snapshot_id, presence: true
     validates :item_id, presence: true, uniqueness: { scope: [:snapshot_id, :item_type] }
-    validates :item_type, presence: true, uniqueness: { scope: [:snapshot_id, :item_id] }
+    validates :item_type, presence: true
 
     def object
       return @object if @object
