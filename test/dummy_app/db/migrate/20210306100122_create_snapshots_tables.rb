2273cf3b4bd74531ca4341602cabed5371bc2eed
Weston Ganger <weston@westonganger.com>
2023-10-17 09:53:53 -0700

    Fix DB migration

--- test/dummy_app/db/migrate/20210306100122_create_snapshots_tables.rb
+++ test/dummy_app/db/migrate/20210306100122_create_snapshots_tables.rb
@@ -8,16 +8,25 @@ class CreateSnapshotsTables < ActiveRecord::Migration::Current
       t.string :identifier, index: true
       t.index [:identifier, :item_id, :item_type], unique: true
 
-      t.<%= ActiveSnapshot.config.storage_method == 'native_json' ? 'json' : 'text' %> :metadata, null: false
+      if ActiveSnapshot.config.storage_method == 'native_json'
+        t.json :metadata, null: false
+      else
+        t.text :metadata, null: false
+      end
 
       t.datetime :created_at, null: false
     end
 
     create_table :snapshot_items do |t|
       t.belongs_to :snapshot, null: false, index: true
-      t.belongs_to :item, polymorphic: true, null: false, unique: [:snapshot_id], index: true
+      t.belongs_to :item, polymorphic: true, null: false, index: true
+      t.index [:snapshot_id, :item_id, :item_type], unique: true
 
-      t.<%= ActiveSnapshot.config.storage_method == 'native_json' ? 'json' : 'text' %> :object, null: false
+      if ActiveSnapshot.config.storage_method == 'native_json'
+        t.json :object, null: false
+      else
+        t.text :object, null: false
+      end
 
       t.datetime :created_at, null: false
       t.string :child_group_name
