10f6859c239d37fbef9520c731c56b3af1d21959
Weston Ganger <weston@westonganger.com>
2023-10-17 09:12:24 -0700

    Fix error generating unique index in DB migration (#43)
    Syntax for creating the index was incorrect

--- test/dummy_app/db/migrate/20210306100122_create_snapshots_tables.rb
+++ test/dummy_app/db/migrate/20210306100122_create_snapshots_tables.rb
@@ -3,14 +3,12 @@ class CreateSnapshotsTables < ActiveRecord::Migration::Current
   def change
     create_table :snapshots do |t|
       t.belongs_to :item, polymorphic: true, null: false, index: true
-      t.string :identifier, unique: true, index: true
       t.belongs_to :user, polymorphic: true
 
-      if ActiveSnapshot.config.storage_method_native_json?
-        t.json :metadata
-      else
-        t.text :metadata
-      end
+      t.string :identifier, index: true
+      t.index [:identifier, :item_id, :item_type], unique: true
+
+      t.<%= ActiveSnapshot.config.storage_method == 'native_json' ? 'json' : 'text' %> :metadata, null: false
 
       t.datetime :created_at, null: false
     end
@@ -19,11 +17,7 @@ class CreateSnapshotsTables < ActiveRecord::Migration::Current
       t.belongs_to :snapshot, null: false, index: true
       t.belongs_to :item, polymorphic: true, null: false, unique: [:snapshot_id], index: true
 
-      if ActiveSnapshot.config.storage_method_native_json?
-        t.json :object, null: false
-      else
-        t.text :object, null: false
-      end
+      t.<%= ActiveSnapshot.config.storage_method == 'native_json' ? 'json' : 'text' %> :object, null: false
 
       t.datetime :created_at, null: false
       t.string :child_group_name
